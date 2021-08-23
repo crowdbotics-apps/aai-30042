0ba462b639d5571ad022670bcd361eebae34251e
Crowdbotics <team@crowdbotics.com>
2021-08-23 11:04:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aai_30042Identifier",
+  url: "https://aai_30042Identifier.botics.co",
+  api: "https://aai_30042Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
